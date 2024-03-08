9e2818cda87252ed9d6d14562dff826035ebcf55
Camila Maia <camila.maia@traderepublic.com>
2024-03-08 20:58:46 +0100

    Arrumando local da pasta templates

--- website/home/views.py
+++ website/home/views.py
@@ -3,7 +3,6 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template("index.html")
-    context = {
-    }
+    template = loader.get_template("home/index.html")
+    context = {}
     return HttpResponse(template.render(context, request))
